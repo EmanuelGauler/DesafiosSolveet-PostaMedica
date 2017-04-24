8a5833bd9929f4bb1a86186114223fe81c8aa2ba
Emanuel Gauler <emanuelgauler@gmail.com>
2017-04-23 21:56:21 -0300

    Se enlazó la licencia a los ficheros fuentes

--- diagnosis-treated.cpp
+++ diagnosis-treated.cpp
@@ -1,3 +1,24 @@
+/*
+ * diagnosis-treated.cpp es la implementación de la interfás DiagnosisTreated. 
+ * Registra las cantidades de pacientes para cada diagnóstico,
+ * y cuando se le solicita realiza el cálculo de las estadísticas para cada diagnóstico.
+ * Copyright (C) 2017 Emanuel Gauler
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact: emanuelgauler@gmail.com
+ */
 #include "DiagnosisTreated"
 using namespace std;
 
--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
+/*
+ * Este el fichero fuente principal del programa. 
+ * Solicita al usuario que ingrese las cantidades de pacientes para cada paciente, 
+ * luego muestra los cálculos realizados por el objeto DiagnosisTreated
+ * Copyright (C) 2017  Emanuel Gauler
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact: emanuelgauler@gmail.com
+ */
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
